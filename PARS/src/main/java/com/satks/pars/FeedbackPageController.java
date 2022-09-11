92f87696aca3785c3c4de88c670daca780bdc13f
sathiyajith <sathiyajith19@gmail.com>
2022-09-11 22:14:54 +0530

    Added boilerplate for Feedback Page

--- PARS/src/main/java/com/satks/pars/FeedbackPageController.java
+++ PARS/src/main/java/com/satks/pars/FeedbackPageController.java
@@ -3,15 +3,23 @@ package com.satks.pars;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.web.WebView;
+import javafx.scene.web.WebEngine;
 
 
 public class FeedbackPageController implements Initializable {
 
+    @FXML
+    private WebView webView;
+    
+    private static WebEngine webEngine;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+        webEngine = webView.getEngine();
+        webEngine.load("file://D:/code/GITHUB_Projects/PARS/PARS/src/main/resources/com/satks/pars/FeedbackPage.html");
     }    
     
 }
